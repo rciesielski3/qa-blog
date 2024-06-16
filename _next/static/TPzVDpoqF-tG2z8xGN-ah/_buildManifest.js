3db65f91b7c7f6bcd422fec37721639687d3265f
r.ciesielski3 <r.ciesielski3@gmail.com>
2024-06-16 21:56:14 +0200

    Updates

---
+++ _next/static/TPzVDpoqF-tG2z8xGN-ah/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],"/posts/[id]":["static/chunks/143-6b819ea65404fd14.js","static/chunks/pages/posts/[id]-4e86004e547aea08.js"],sortedPages:["/_app","/_error","/posts/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
