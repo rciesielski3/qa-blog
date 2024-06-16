1fe2bdd223df8965be2e2b7242d7c9d5cc754914
r.ciesielski3 <r.ciesielski3@gmail.com>
2024-06-16 18:10:18 +0200

    refactor next.config file for gh action

--- sample-blog/next.config.mjs
+++ sample-blog/next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  reactStrictMode: true,
   basePath: isProd ? "/sampleBlog/sample-blog" : "",
   assetPrefix: isProd ? "/sampleBlog/sample-blog/" : "",
 };
