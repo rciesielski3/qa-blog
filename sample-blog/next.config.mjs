e151968462a11aeb57eb02ba6b4bc4480924380e
r.ciesielski3 <r.ciesielski3@gmail.com>
2024-06-16 18:09:04 +0200

    refactor deploy scripts

--- sample-blog/next.config.mjs
+++ sample-blog/next.config.mjs
@@ -2,4 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
 };
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  basePath: isProd ? "/sampleBlog/sample-blog" : "",
+  assetPrefix: isProd ? "/sampleBlog/sample-blog/" : "",
+};
 export default nextConfig;
