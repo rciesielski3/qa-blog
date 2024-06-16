0a9cdb69205b1d16c0d8796419e932df3d5089cd
r.ciesielski3 <r.ciesielski3@gmail.com>
2024-06-16 20:17:02 +0200

    move files to upper folder, change name

--- sample-blog/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  reactStrictMode: true,
-  basePath: isProd ? "/sampleBlog/sample-blog" : "",
-  assetPrefix: isProd ? "/sampleBlog/sample-blog/" : "",
-};
-export default nextConfig;
